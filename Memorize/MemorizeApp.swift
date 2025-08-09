21393b986c6e86732b533afcf1f0b12bab2545d4
Kelly <kellogilee@gmail.com>
2025-08-09 14:26:24 -0300

    first commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,32 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kelly Gihee Lee da Silva on 15/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MemorizeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
